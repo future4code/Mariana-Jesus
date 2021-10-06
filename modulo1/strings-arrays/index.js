9cee731adbbdf68f4b8791899083f236c52f0bf7
Mariana Jesus <marianadiscipula@gmail.com>
2021-10-06 15:41:28 -0300

    Adição de strings-arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,89 @@
+//1
+
+        //console.log('a.', array) = a. undefined
+
+        //console.log('b.', array) = b. null
+
+        //console.log('c.', array) = c. 11
+
+        //console.log('d.', array) = d. 3
+
+        //console.log('e.', array) = e. [3, 19, 5...]
+
+        //console.log('f.', array) = f. 9
+
+
+
+//2
+
+        // SUBI NUM ÔNIBUS EM MIRROCOS 27
+        
+
+        
+//------------------------------
+
+
+
+//1
+/*
+        let emailDoUsuario = prompt('Digite seu e-mail:')
+        let nomeDoUsuario = prompt('Digite seu nome:')
+
+        console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem vindo(a), ${nomeDoUsuario}`)
+*/
+
+
+
+//2
+/*
+        let comidasFavoritas = ['Purê de batata', 'Churrasco', 'Salpicão', 'Açaí', 'Feijoada']
+
+        // console.log(comidasFavoritas)
+
+        // console.log(`Essas são as minhas comidas preferidas: 
+        // ${comidasFavoritas[0]}
+        // ${comidasFavoritas[1]}
+        // ${comidasFavoritas[2]}
+        // ${comidasFavoritas[3]}
+        // ${comidasFavoritas[4]}`)
+
+        let troca = prompt('Comida favorita:')
+    
+        console.log(
+        comidasFavoritas[0], '\n',
+        comidasFavoritas[1] = troca, '\n',
+        comidasFavoritas[2], '\n',
+        comidasFavoritas[3], '\n',
+        comidasFavoritas[4]
+            )
+*/
+
+
+//3
+/*
+        let listaDeTarefas = []
+
+        listaDeTarefas[0] = prompt('Escreva a primeira tarefa: ')
+        listaDeTarefas[1] = prompt('Escreva a quarta tarefa: ')
+        listaDeTarefas[2] = prompt('Escreva a terceira tarefa: ')
+            console.log(listaDeTarefas)
+
+        let i = Number(prompt('Digite o ínsice de uma das tarefas:'))
+                console.log(listaDeTarefas.splice(i, 1))
+*/
+
+
+//Desafios
+/*
+     1
+let frase = 'Oi, como vai?'
+let array = frase.split(' ')
+console.log(array)
+*/
+
+//2
+/*
+let array = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+
+console.log(array.indexOf('Abacaxi'), array.length)
+*/
