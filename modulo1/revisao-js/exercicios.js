d449c6d48fe21c82972e69391f2ffe7b006e6569
Mariana Jesus <marianadiscipula@gmail.com>
2021-10-18 14:29:52 -0300

    Exercício 1

---
+++ modulo1/revisao-js/exercicios.js
@@ -0,0 +1,120 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+
+function retornaTamanhoArray(array) {
+   return array.length
+}retornaTamanhoArray([3, 2, 1, 4, 7])
+
+
+/*
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+  return array.slice(0).reverse()
+}
+retornaArrayInvertido([0,8,23,16,10,15,41,12,13])
+*/
+
+// EXERCÍCIO 03
+/*
+const comparar = (a, b) =>{
+    return a - b
+}
+
+function retornaArrayOrdenado(array) {
+    const comparacao = comparar
+  return array.sort(comparacao)
+}retornaArrayOrdenado([36, 12, 56, 7, 3])
+
+*/
+
+// EXERCÍCIO 04
+/*
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8]
+function retornaNumerosPares(array) {
+    array = array1.filter(numerosPares)
+    return array
+}
+
+const numerosPares = (par) => {
+    return par % 2 === 0
+}
+retornaNumerosPares([])
+
+// EXERCÍCIO 05
+const retornaNumerosParesElevadosADois = retornaNumerosPares.map((array) => {
+    
+    return array
+})
+console.log(retornaNumerosParesElevadosADois)
+*/
+
+
+
+
+/*
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  let arr = [1, 2, 3, 4, 5, 6, 7]
+  array = Math.max(...arr)
+  return array
+}
+retornaMaiorNumero()
+*/
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
