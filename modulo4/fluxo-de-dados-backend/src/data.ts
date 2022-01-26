d24a113c1aee48bcc6f7c70f006cbad45e28e665
Mariana <marianadiscipula@gmail.com>
2022-01-26 18:30:58 -0300

    Fluxo de dados

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,37 @@
+type PRODUTOS = {
+    id: string,
+    name: string,
+    price: number
+}
+export const produtos:PRODUTOS[] = [
+    {
+        id: '1',
+        name: 'Assassinato no expresso do orinte',
+        price: 25
+    },
+    {
+        id: '2',
+        name: 'Volta ao mundo em 80 dias',
+        price: 27
+    },
+    {
+        id: '3',
+        name: 'O caso dos dez negrinhos',
+        price: 30
+    },
+    {
+        id: '4',
+        name: '12 regras para a vida',
+        price: 45
+    },
+    {
+        id: '5',
+        name: 'O Peregrino',
+        price: 50
+    },
+    {
+        id: '6',
+        name: 'Viagem ao centro da terra',
+        price: 20
+    },
+]
