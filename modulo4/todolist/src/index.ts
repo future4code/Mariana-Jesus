f32570ce5b1c5c2a3efc0cc94853e4f1bb504f1c
Mariana <marianadiscipula@gmail.com>
2022-02-06 19:38:28 -0300

    Final

--- modulo4/todolist/src/index.ts
+++ modulo4/todolist/src/index.ts
@@ -1,16 +1,21 @@
-import express from "express";
-
-import { AddressInfo } from "net";
+import express, { Request, Response } from "express";
+import getUserById from "./endpoints/getByid";
+import editUser from "./endpoints/editUser";
+import createUser from "./endpoints/createUser";
+import createTask from "./endpoints/createTaskes";
 
 const app = express();
 
 app.use(express.json());
 
-const server = app.listen(process.env.PORT || 3003, () => {
-  if (server) {
-    const address = server.address() as AddressInfo;
-    console.log(`Server is running in http://localhost:${address.port}`);
-  } else {
-    console.error(`Failure upon starting server.`);
-  }
+app.put('/user', createUser)
+
+app.get('/user/:id', getUserById)
+
+app.post('/user/edit/:id', editUser)
+
+app.put('/task', createTask)
+
+app.listen(3003, ()=>{
+  console.log('Foi')
 })
