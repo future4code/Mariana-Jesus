be996ea1015d0353bc5b250ea58b25678f0d9383
Mariana <marianadiscipula@gmail.com>
2022-02-23 17:43:43 -0300

    Mudando

--- modulo5/criptografia-e-user-roles/src/index.ts
+++ modulo5/criptografia-e-user-roles/src/index.ts
@@ -6,6 +6,40 @@ const app = express();
 
 app.use(express.json());
 
+
+// app.post("/signup", async (req: Request, res: Response) => {
+//     try {
+//       const userData = {
+//         email: req.body.email,
+//         password: req.body.password
+//       };
+    
+//       const id = generate();
+  
+//       const hashPassword = await hash(userData.password);
+  
+//       await createUser(id, userData.email, hashPassword);
+  
+//       const token = generateToken({
+//         id
+//       });
+  
+//       res.status(200).send({
+//         token,
+//       });
+//     } catch (err) {
+//       res.status(400).send({
+//         message: err.message,
+//       });
+//     }
+//   });
+
+
+
+
+  app.post("/login",)
+
+
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
